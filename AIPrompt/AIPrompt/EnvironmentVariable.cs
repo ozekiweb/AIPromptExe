775a00076902683e4ff4a04b8fbfe79feca0ed0d
Domina Márk Zsolt <71927541+a20th@users.noreply.github.com>
2025-01-10 09:15:39 +0100

    fix: EnvironmentVariable read
    EnvironmentVariables did not check for user variables

--- AIPrompt/AIPrompt/EnvironmentVariable.cs
+++ AIPrompt/AIPrompt/EnvironmentVariable.cs
@@ -8,14 +8,22 @@ namespace Ozeki
 {
     internal static class EnvironmentVariable
     {
-        public static readonly string? URL = Environment.GetEnvironmentVariable("OZEKI_AIPROMPT_URL");
+        public static readonly string? URL = TestAllLocation("OZEKI_AIPROMPT_URL");
 
-        public static readonly string? USERNAME = Environment.GetEnvironmentVariable("OZEKI_AIPROMPT_USERNAME");
+        public static readonly string? USERNAME = TestAllLocation("OZEKI_AIPROMPT_USERNAME");
         
-        public static readonly string? PASSWORD = Environment.GetEnvironmentVariable("OZEKI_AIPROMPT_PASSWORD");
+        public static readonly string? PASSWORD = TestAllLocation("OZEKI_AIPROMPT_PASSWORD");
 
-        public static readonly string? APIKEY = Environment.GetEnvironmentVariable("OZEKI_AIPROMPT_APIKEY");
+        public static readonly string? APIKEY = TestAllLocation("OZEKI_AIPROMPT_APIKEY");
 
-        public static readonly string? USE_JSON = Environment.GetEnvironmentVariable("OZEKI_AIPROMPT_USE_JSON");
+        public static readonly string? USE_JSON = TestAllLocation("OZEKI_AIPROMPT_USE_JSON");
+
+        private static string? TestAllLocation(string variable)
+        {
+            return Environment.GetEnvironmentVariable(variable, EnvironmentVariableTarget.Process)
+                ?? Environment.GetEnvironmentVariable(variable, EnvironmentVariableTarget.User)
+                ?? Environment.GetEnvironmentVariable(variable, EnvironmentVariableTarget.Machine);
+        }
+
     }
 }
